8b6eb391a07111793190da4b43e9246b8823b1ef
Natalia Kowalczyk <melitele@code42day.com>
2025-03-09 00:06:34 -0700

    adding default empty name for features with no name
    apparently evaluating 'coalesce' will result in a warning
    if all its predicates evaluate to null

--- test/localize.js
+++ test/localize.js
@@ -6,5 +6,5 @@ test('localize with language', () => {
   assert.ok(localize() === undefined);
 
   // takes language into account for name
-  assert.deepEqual(['coalesce', ['get', 'name:en'], ['get', 'name:latin'], ['get', 'name_int'], ['get', 'name']], localize('en'));
+  assert.deepEqual(['coalesce', ['get', 'name:en'], ['get', 'name:latin'], ['get', 'name_int'], ['get', 'name'], ''], localize('en'));
 });
