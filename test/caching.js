727c245d99b3a73a8b8e858177ef3d91176434a4
Natalia Kowalczyk <melitele@code42day.com>
2025-03-26 22:29:57 -0600

    expect evaluator for caching expressions to be provided

--- test/caching.js
+++ test/caching.js
@@ -5,17 +5,23 @@ const { initCaching, getStrategy } = require('../lib/caching');
 test('caching default', function () {
   const caching = initCaching({
     default: 'do-nothing'
-  });
+  }, caching => (() => caching.default));
 
   const strategy = getStrategy(caching, {});
   assert.equal(strategy, 'do-nothing');
 });
 
+function evalCaching(caching) {
+  const { 'default': defValue } = caching;
+  caching = Object.entries(caching);
+  return options => caching.find(([k, v]) => options[v])?.[0] ?? defValue;
+}
+
 test('caching single option', function () {
   let caching = initCaching({
     default: 'do-nothing',
     'network-only': 'field'
-  });
+  }, evalCaching);
 
   let strategy = getStrategy(caching, {
     field: true
@@ -31,7 +37,7 @@ test('caching multiple options', function () {
     default: 'do-nothing',
     'network-only': 'field1',
     'cache-only': 'field2'
-  });
+  }, evalCaching);
 
   let strategy = getStrategy(caching, {
     field1: true
@@ -46,3 +52,4 @@ test('caching multiple options', function () {
   strategy = getStrategy(caching, {});
   assert.equal(strategy, 'do-nothing');
 });
+
